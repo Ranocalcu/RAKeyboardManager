08f09f21d84d28483f4e4bd6a4e7606100e446bb
guopeng <guopeng02@58.com>
2015-11-26 16:52:18 +0800

    Initial Commit

---
+++ RAKeyBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RAKeyBoard
+//
+//  Created by rano on 15/11/26.
+//  Copyright © 2015年 rano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
